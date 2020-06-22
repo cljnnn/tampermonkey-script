b82d7f9d3d99969370aa7037e9c926ecce71d4ba
cljnnn <cljnnn@gmail.com>
2020-06-22 16:11:39 +0800

    work for chrome

--- v2ex/sort.js
+++ v2ex/sort.js
@@ -4,11 +4,19 @@
 // @include     http*://*v2ex.com/t/*
 // @description:en sort v2ex reply by star❤️
 // @description:zh-CN v2ex回复按照❤️排序
-// @version     1.0.0.4
+// @version     1.0.0.5
 // @description sort v2ex reply by star❤️
 // ==/UserScript==
+
+//green author
+$("strong a.dark:contains('"+$("small.gray a").text()+"')").css('color', 'green');
+
+//blue link
+//$("div.cell[id^='r_'] a[rel='nofollow']").css('color', 'blue');
+$("a[rel~='nofollow']").css('color', 'blue');
+
 let content = $("div.box[style] + div + div.box");
-let hasPages = content.find("a.page_current").size()!==0;
+let hasPages = content.find("a.page_current").length!==0;
 let lastPageRow = hasPages ? content.find("div.cell:last") : null;
 content.find("div.cell[id^='r_']").sort(
     function(x, y){
@@ -16,19 +24,11 @@ content.find("div.cell[id^='r_']").sort(
         if (isNaN(vx)) vx = 0;
         let vy = parseInt($(y).find("span.small").text().replace(/♥/g, ""));
         if (isNaN(vy)) vy = 0;
-        return vx <= vy;
+        return vy - vx;
     }
 ).appendTo(content);
-
 
 // move the lastPageRow to the end of content
 if(lastPageRow){
     lastPageRow.appendTo(content);
 }
-
-//green author
-$("strong a.dark:contains('"+$("small.gray a").text()+"')").css('color', 'green');
-
-//blue link
-//$("div.cell[id^='r_'] a[rel='nofollow']").css('color', 'blue');
-$("a[rel='nofollow']").css('color', 'blue');

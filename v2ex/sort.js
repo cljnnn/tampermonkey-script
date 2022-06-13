1071f64cefa133b2d08f93bbde6549c2e374b2ea
MrChen <cljnnn@gmail.com>
2022-06-13 08:40:00 +0800

    remove jquery, rewirte

--- v2ex/sort.js
+++ v2ex/sort.js
@@ -2,33 +2,51 @@
 // @name        v2ex回复排序
 // @namespace   cljnnn@gmail.com
 // @include     http*://*v2ex.com/t/*
-// @description:en sort v2ex reply by star❤️
-// @description:zh-CN v2ex回复按照❤️排序，如果你不想花太多时间在v2ex上，但又不想错过重要信息，你可能需要这个。
-// @version     1.0.0.6
-// @description sort v2ex reply by star❤️
+// @description:en sort v2ex replies by star❤️
+// @description:zh-CN v2ex回复按照❤️排序，如果你不想花太多时间在v2ex上，但又不想错过重要信息，你可能需要这个。兼容 V2EX Pro
+// @version     1.1.0
+// @description sort v2ex replies by star❤️, compatible with V2EX Pro
 // ==/UserScript==
 
-//green author
-$("strong a.dark:contains('"+$("small.gray a").text()+"')").css('color', 'green');
-
-//blue link
-//$("div.cell[id^='r_'] a[rel='nofollow']").css('color', 'blue');
-$("a[rel~='nofollow']").css('color', 'blue');
-
-let content = $("div.box[style] + div + div.box:not(#reply-box):has(div.cell)");
-let hasPages = content.find("a.page_current").length!==0;
-let lastPageRow = hasPages ? content.find("div.cell:last") : null;
-content.find("div.cell[id^='r_']").sort(
-    function(x, y){
-        let vx = parseInt($(x).find("span.small").text().replace(/♥/g, ""));
-        if (isNaN(vx)) vx = 0;
-        let vy = parseInt($(y).find("span.small").text().replace(/♥/g, ""));
-        if (isNaN(vy)) vy = 0;
-        return vy - vx;
+// green poster
+let posterName = document.querySelector("small.gray a").textContent;
+for (let author of document.querySelectorAll("strong a.dark")) {
+    if (author.textContent.includes(posterName)) {
+        author.style.color = "green";
     }
-).appendTo(content);
+}
 
-// move the lastPageRow to the end of content
-if(lastPageRow){
-    lastPageRow.appendTo(content);
+// blue link
+for (let link of document.querySelectorAll("a[rel~='nofollow']")) {
+    link.style.color = "blue";
+}
+
+// sort likes, compatible with V2EX Pro
+let thread = document.querySelector("#Main > div.box:nth-child(4)");
+let replies = thread.querySelectorAll(":scope > div.cell[id]");
+
+let replyArray = [];
+for (let reply of replies) {
+    replyArray.push(reply);
+}
+
+let countLike = function (reply) {
+    let count = 0;
+    let likes = reply.querySelectorAll("span.small")
+    for (let like of likes) {
+        count += parseInt(like.textContent);
+    }
+    return count;
+}
+replyArray.sort(function (x, y) {
+    return countLike(y) - countLike(x);
+})
+for (let reply of replyArray) {
+    thread.appendChild(reply);
+}
+
+// handle page rows
+let pageRows = thread.querySelectorAll("div.cell[style]");
+if (pageRows.length > 0) {
+    thread.appendChild(pageRows[pageRows.length - 1]);
 }
